2ae6d5a837935f73a8357ce4bc695edad624d22b
FilipJQ77 <50529336+FilipJQ77@users.noreply.github.com>
2020-06-07 01:15:31 +0200

    Add files via upload

--- Java/Main.java
+++ Java/Main.java
@@ -1,134 +1,13 @@
-package com.company;
-
 import java.io.*;
-import java.lang.reflect.Array;
-import java.net.Inet4Address;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     static int repNr = 10;
 
-    static void isOk (List<Integer> arr)
-    {
-        for (int i = 0; i < arr.size()-1; i++){
-            if (arr.get(i) > arr.get(i+1)){
-                System.out.println("Cos nie pyklo");
-                break;
-            }
-        }
-    }
-
-    static void isOk (int arr[])
-    {
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i] > arr[i+1]){
-                System.out.println("Cos nie pyklo");
-                break;
-            }
-        }
-    }
-
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
-        System.out.println();
-    }
-
-    static void printList(List<Integer> lista)
-    {
-        for (int i : lista){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
-
-    static void testTime (LinkedList<Integer> arr)
-    {
-        LinkedList<Integer> copy = new LinkedList<>(arr);
-        long startTime;
-        long estimatedTime0 = 0, estimatedTime1 = 0, estimatedTime2 = 0,
-                estimatedTime3 = 0, estimatedTime4 = 0, estimatedTime5 = 0,
-                estimatedTime6 = 0, estimatedTime7 = 0, estimatedTime8 = 0,
-                estimatedTime9 = 0;
-
-        for (int i = 0; i < repNr; i++) {
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.bubbleSort(copy);
-            estimatedTime0 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.insertionSort(copy);
-            estimatedTime1 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.selectionSort(copy);
-            estimatedTime2 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.quickSortRec(copy);
-            estimatedTime3 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.quickSortIter(copy);
-            estimatedTime4 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.topDownMergeSort(copy);
-            estimatedTime5 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.bottomUpMergeSort(copy);
-            estimatedTime6 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.shellSort(copy);
-            estimatedTime7 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Sort.heapSort(copy);
-            estimatedTime8 += System.nanoTime() - startTime;
-
-            copy = new LinkedList<>(arr);
-            startTime = System.nanoTime();
-            Collections.sort(copy);
-            estimatedTime9 += System.nanoTime() - startTime;
-        }
-
-        try {
-            FileWriter myWriter = new FileWriter("LinkedList.txt");
-            myWriter.write("LinkedList - bubbleSort = " + estimatedTime0/repNr + "us");
-            myWriter.write("\nLinkedList - insertionSort = " + estimatedTime1/repNr + "us");
-            myWriter.write("\nLinkedList - selectionSort = " + estimatedTime2/repNr + "us");
-            myWriter.write("\nLinkedList - quickSortRecursive = " + estimatedTime3/repNr + "us");
-            myWriter.write("\nLinkedList - quickSortIterative = " + estimatedTime4/repNr + "us");
-            myWriter.write("\nLinkedList - topDownMergeSort = " + estimatedTime5/repNr + "us");
-            myWriter.write("\nLinkedList - bottomUpMergeSort = " + estimatedTime6/repNr + "us");
-            myWriter.write("\nLinkedList - shellSort = " + estimatedTime7/repNr + "us");
-            myWriter.write("\nLinkedList - heapSort = " + estimatedTime8/repNr + "us");
-            myWriter.write("\nLinkedList - librarySort = " + estimatedTime9/repNr + "us");
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
-    static void testTime (ArrayList<Integer> arr)
-    {
-        ArrayList<Integer> copy = new ArrayList<>(arr);
+    static void testTime(ArrayList<Integer> arr) {
+        ArrayList<Integer> copy;
         long startTime;
         long estimatedTime0 = 0, estimatedTime1 = 0, estimatedTime2 = 0,
                 estimatedTime3 = 0, estimatedTime4 = 0, estimatedTime5 = 0,
@@ -190,16 +69,16 @@ public class Main {
 
         try {
             FileWriter myWriter = new FileWriter("ArrayList.txt");
-            myWriter.write("ArrayList - bubbleSort = " + estimatedTime0/repNr + "us");
-            myWriter.write("\nArrayList - insertionSort = " + estimatedTime1/repNr + "us");
-            myWriter.write("\nArrayList - selectionSort = " + estimatedTime2/repNr + "us");
-            myWriter.write("\nArrayList - quickSortRecursive = " + estimatedTime3/repNr + "us");
-            myWriter.write("\nArrayList - quickSortIterative = " + estimatedTime4/repNr + "us");
-            myWriter.write("\nArrayList - topDownMergeSort = " + estimatedTime5/repNr + "us");
-            myWriter.write("\nArrayList - bottomUpMergeSort = " + estimatedTime6/repNr + "us");
-            myWriter.write("\nArrayList - shellSort = " + estimatedTime7/repNr + "us");
-            myWriter.write("\nArrayList - heapSort = " + estimatedTime8/repNr + "us");
-            myWriter.write("\nArrayList - librarySort = " + estimatedTime9/repNr + "us");
+            myWriter.write((double) estimatedTime0 / repNr + "\n");
+            myWriter.write((double) estimatedTime1 / repNr + "\n");
+            myWriter.write((double) estimatedTime2 / repNr + "\n");
+            myWriter.write((double) estimatedTime3 / repNr + "\n");
+            myWriter.write((double) estimatedTime4 / repNr + "\n");
+            myWriter.write((double) estimatedTime5 / repNr + "\n");
+            myWriter.write((double) estimatedTime6 / repNr + "\n");
+            myWriter.write((double) estimatedTime7 / repNr + "\n");
+            myWriter.write((double) estimatedTime8 / repNr + "\n");
+            myWriter.write((double) estimatedTime9 / repNr + "\n");
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
@@ -208,15 +87,15 @@ public class Main {
         }
     }
 
-    static void testTime (int arr[])
-    {
-        int copy[] = arr.clone();
+    static void testTime(int arr[]) {
+        int copy[];
         long startTime;
         long estimatedTime0 = 0, estimatedTime1 = 0, estimatedTime2 = 0,
                 estimatedTime3 = 0, estimatedTime4 = 0, estimatedTime5 = 0,
                 estimatedTime6 = 0, estimatedTime7 = 0, estimatedTime8 = 0,
                 estimatedTime9 = 0;
         for (int i = 0; i < repNr; i++) {
+            copy = arr.clone();
             startTime = System.nanoTime();
             Sort.bubbleSort(copy);
             estimatedTime0 += System.nanoTime() - startTime;
@@ -268,16 +147,16 @@ public class Main {
         }
         try {
             FileWriter myWriter = new FileWriter("JavaArray.txt");
-            myWriter.write("Array - bubbleSort = " + estimatedTime0/repNr + "us");
-            myWriter.write("\nArray - insertionSort = " + estimatedTime1/repNr + "us");
-            myWriter.write("\nArray - selectionSort = " + estimatedTime2/repNr + "us");
-            myWriter.write("\nArray - quickSortRecursive = " + estimatedTime3/repNr + "us");
-            myWriter.write("\nArray - quickSortIterative = " + estimatedTime4/repNr + "us");
-            myWriter.write("\nArray - topDownMergeSort = " + estimatedTime5/repNr + "us");
-            myWriter.write("\nArray - bottomUpMergeSort = " + estimatedTime6/repNr + "us");
-            myWriter.write("\nArray - shellSort = " + estimatedTime7/repNr + "us");
-            myWriter.write("\nArray - heapSort = " + estimatedTime8/repNr + "us");
-            myWriter.write("\nArray - librarySort = " + estimatedTime9/repNr + "us");
+            myWriter.write((double) estimatedTime0 / repNr + "\n");
+            myWriter.write((double) estimatedTime1 / repNr + "\n");
+            myWriter.write((double) estimatedTime2 / repNr + "\n");
+            myWriter.write((double) estimatedTime3 / repNr + "\n");
+            myWriter.write((double) estimatedTime4 / repNr + "\n");
+            myWriter.write((double) estimatedTime5 / repNr + "\n");
+            myWriter.write((double) estimatedTime6 / repNr + "\n");
+            myWriter.write((double) estimatedTime7 / repNr + "\n");
+            myWriter.write((double) estimatedTime8 / repNr + "\n");
+            myWriter.write((double) estimatedTime9 / repNr + "\n");
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
@@ -286,247 +165,33 @@ public class Main {
         }
     }
 
-    static void testRam (LinkedList<Integer> list) throws IOException {
-
-        System.gc();
-        LinkedList<Integer> copy = new LinkedList<>(list);
-        System.out.println("LinkedList - bubbleSort");
-        System.in.read();
-        Sort.bubbleSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - insertionSort");
-        System.in.read();
-        Sort.insertionSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - selectionSort");
-        System.in.read();
-        Sort.selectionSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - quickSortRec");
-        System.in.read();
-        Sort.quickSortRec(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - quickSortIter");
-        System.in.read();
-        Sort.quickSortIter(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - topDownMergeSort");
-        System.in.read();
-        Sort.topDownMergeSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - bottomUpMergeSort");
-        System.in.read();
-        Sort.bottomUpMergeSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - shellSort");
-        System.in.read();
-        Sort.shellSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - heapSort");
-        System.in.read();
-        Sort.heapSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new LinkedList<>(list);
-        System.out.println("LinkedList - library");
-        System.in.read();
-        Collections.sort(copy);
-    }
-
-    static void testRam (ArrayList<Integer> list) throws IOException {
-
-        System.gc();
-        ArrayList<Integer> copy = new ArrayList<>(list);
-        System.out.println("ArrayList - bubbleSort");
-        System.in.read();
-        Sort.bubbleSort(copy);
-
-        System.in.read();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - insertionSort");
-        System.in.read();
-        Sort.insertionSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - selectionSort");
-        System.in.read();
-        Sort.selectionSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - quickSortRec");
-        System.in.read();
-        Sort.quickSortRec(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - quickSortIter");
-        System.in.read();
-        Sort.quickSortIter(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - topDownMergeSort");
-        System.in.read();
-        Sort.topDownMergeSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - bottomUpMergeSort");
-        System.in.read();
-        Sort.bottomUpMergeSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - shellSort");
-        System.in.read();
-        Sort.shellSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - heapSort");
-        System.in.read();
-        Sort.heapSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = new ArrayList<>(list);
-        System.out.println("ArrayList - library");
-        System.in.read();
-        Collections.sort(copy);
-    }
-
-    static void testRam (int arr[]) throws IOException {
-
-        System.gc();
-        int copy[] = arr.clone();
-        System.out.println("Array - bubbleSort");
-        System.in.read();
-        Sort.bubbleSort(copy);
-
-        System.in.read();
-        copy = arr.clone();
-        System.out.println("Array - insertionSort");
-        System.in.read();
-        Sort.insertionSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - selectionSort");
-        System.in.read();
-        Sort.selectionSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - quickSortRec");
-        System.in.read();
-        Sort.quickSortRec(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - quickSortIter");
-        System.in.read();
-        Sort.quickSortIter(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - topDownMergeSort");
-        System.in.read();
-        Sort.topDownMergeSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - bottomUpMergeSort");
-        System.in.read();
-        Sort.bottomUpMergeSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - shellSort");
-        System.in.read();
-        Sort.shellSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - heapSort");
-        System.in.read();
-        Sort.heapSort(copy);
-
-        System.in.read();
-        System.gc();
-        copy = arr.clone();
-        System.out.println("Array - library");
-        System.in.read();
-        Arrays.sort(copy);
-    }
-
-    static ArrayList<Integer> read(int n) throws IOException {
+    static ArrayList<Integer> readArrList(int n, String filename) throws IOException {
         ArrayList<Integer> arr = new ArrayList<>();
-        File fil = new File("liczby.txt");
+        File fil = new File(filename);
         FileReader inputFil = new FileReader(fil);
         BufferedReader in = new BufferedReader(inputFil);
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             arr.add(Integer.parseInt(in.readLine()));
+        }
+        return arr;
+    }
+
+    static int[] readArr(int n, String filename) throws IOException {
+        int[] arr = new int[n];
+        File fil = new File(filename);
+        FileReader inputFil = new FileReader(fil);
+        BufferedReader in = new BufferedReader(inputFil);
+        for (int i = 0; i < n; i++) {
+            arr[i] = (Integer.parseInt(in.readLine()));
         }
         return arr;
     }
 
     public static void main(String[] args) throws IOException {
 
-        ArrayList<Integer> arr = read(10);
-        LinkedList<Integer> copy = new LinkedList<>(arr);
-
-        int array[] = new int[arr.size()];
-        for (int i = 0; i < arr.size(); i++){
-            array[i] = arr.get(i).intValue();
-        }
-        testTime(array);
+        ArrayList<Integer> arr = readArrList(10000, "plik");
         testTime(arr);
-        testTime(copy);
-            // SORTOWANIE Z BIBLIOTEK
-//        Arrays.sort(arr); // DO TABLICY
-//        printArray(arr);
-//        Collections.sort(lista2); // DO LIST
-//        printList(lista2);
+        int[] arrr = readArr(10000, "plik");
+        testTime(arrr);
     }
 }
--- Java/Sort.java
+++ Java/Sort.java
@@ -1,6 +1,4 @@
-package com.company;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -81,14 +79,11 @@ public class Sort {
         int n = arr.size();
         for (int i = 0; i < n-1; i++)
         {
-            // Find the minimum element in unsorted array
             int min_idx = i;
             for (int j = i+1; j < n; j++)
                 if (arr.get(j) < arr.get(min_idx))
                     min_idx = j;
 
-            // Swap the found minimum element with the first
-            // element
             if (min_idx != i) {
                 int temp = arr.get(min_idx);
                 arr.set(min_idx, arr.get(i));
@@ -102,14 +97,11 @@ public class Sort {
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
         {
-            // Find the minimum element in unsorted array
             int min_idx = i;
             for (int j = i+1; j < n; j++)
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
 
-            // Swap the found minimum element with the first
-            // element
             if (min_idx != i) {
                 int temp = arr[min_idx];
                 arr[min_idx] = arr[i];
@@ -132,32 +124,22 @@ public class Sort {
     {
         int l = 0;
         int h = arr.size() - 1;
-        // Create an auxiliary stack
         Stack stack = new Stack();
 
-        // push initial values of l and h to stack
         stack.push(l);
         stack.push(h);
 
-        // Keep popping from stack while is not empty
         while (!stack.empty()) {
-            // Pop h and l
             h = (int) stack.pop();
             l = (int) stack.pop();
 
-            // Set pivot element at its correct position
-            // in sorted array
             int p = SortHelper.qsPartition(arr, l, h);
 
-            // If there are elements on left side of pivot,
-            // then push left side to stack
             if (p - 1 > l) {
                 stack.push(l);
                 stack.push(p-1);
             }
 
-            // If there are elements on right side of pivot,
-            // then push right side to stack
             if (p + 1 < h) {
                 stack.push(p+1);
                 stack.push(h);
@@ -169,32 +151,20 @@ public class Sort {
     {
         int l = 0;
         int h = arr.length - 1;
-        // Create an auxiliary stack
         Stack stack = new Stack();
 
-        // push initial values of l and h to stack
         stack.push(l);
         stack.push(h);
 
-        // Keep popping from stack while is not empty
         while (!stack.empty()) {
-            // Pop h and l
             h = (int) stack.pop();
             l = (int) stack.pop();
-
-            // Set pivot element at its correct position
-            // in sorted array
             int p = SortHelper.qsPartition(arr, l, h);
 
-            // If there are elements on left side of pivot,
-            // then push left side to stack
             if (p - 1 > l) {
                 stack.push(l);
                 stack.push(p-1);
             }
-
-            // If there are elements on right side of pivot,
-            // then push right side to stack
             if (p + 1 < h) {
                 stack.push(p+1);
                 stack.push(h);
@@ -214,29 +184,11 @@ public class Sort {
         SortHelper.topDownSplitMerge(working_arr, 0, arr.size(), arr);
     }
 
-    static LinkedList<Integer> topDownMergeSort (LinkedList<Integer> arr)
-    {
-        if (arr.size() <= 1)
-            return arr;
-        LinkedList left = new LinkedList();
-        LinkedList right = new LinkedList();
-        for (int i = 0; i < arr.size(); i++){
-            if (i < arr.size()/2) {
-                left.add(arr.get(i));
-            } else
-                right.add(arr.get(i));
-        }
-        left = topDownMergeSort(left);
-        right = topDownMergeSort(right);
-        return SortHelper.merge(left,right);
-    }
-
     static void bottomUpMergeSort(int arr[])
     {
         int n = arr.length;
         int curr_size;
         int left_start;
-        //int working_arr[] = arr.clone();
         for (curr_size = 1; curr_size < n; curr_size *= 2){
             for (left_start = 0; left_start < n; left_start += 2*curr_size){
                 SortHelper.bottomUpMerge (arr, left_start, Math.min(left_start+curr_size - 1, n - 1), Math.min(left_start+2*curr_size - 1,n - 1));
@@ -249,7 +201,6 @@ public class Sort {
         int n = arr.size();
         int curr_size;
         int left_start;
-        //int working_arr[] = arr.clone();
         for (curr_size = 1; curr_size < n; curr_size *= 2){
             for (left_start = 0; left_start < n; left_start += 2*curr_size){
                 SortHelper.bottomUpMerge (arr, left_start, Math.min(left_start+curr_size - 1, n - 1), Math.min(left_start+2*curr_size - 1,n - 1));
@@ -262,7 +213,7 @@ public class Sort {
         int gap = n / 2;
         while (gap > 0)
         {
-            for (int i = gap; i < n; i++) //in range(gap, n):
+            for (int i = gap; i < n; i++)
             {
                 int temp = arr[i];
                 int j = i;
@@ -282,7 +233,7 @@ public class Sort {
         int gap = n / 2;
         while (gap > 0)
         {
-            for (int i = gap; i < n; i++) //in range(gap, n):
+            for (int i = gap; i < n; i++)
             {
                 int temp = arr.get(i);
                 int j = i;
@@ -323,4 +274,3 @@ public class Sort {
         }
     }
 }
-
